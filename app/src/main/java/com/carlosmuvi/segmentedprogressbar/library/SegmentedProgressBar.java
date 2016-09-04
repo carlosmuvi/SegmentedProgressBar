e90d731c42e946199170cca6c26ff363eb63f7e8
carlosmuvi <kalospiles@gmail.com>
2016-09-04 12:51:40 +0200

    Rounded borders.

--- app/src/main/java/com/carlosmuvi/segmentedprogressbar/library/SegmentedProgressBar.java
+++ app/src/main/java/com/carlosmuvi/segmentedprogressbar/library/SegmentedProgressBar.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -80,7 +81,7 @@ public class SegmentedProgressBar extends View {
         int botY = getHeight();
 
         for (int i = 0; i < SEGMENT_COUNT; i++) {
-            canvas.drawRect(leftX, topY, rightX, botY, containerRectanglePaint);
+            drawRoundedRect(canvas, leftX, topY, rightX, botY, containerRectanglePaint);
             leftX = leftX + segmentWidth;
             rightX = rightX + segmentWidth;
         }
@@ -95,7 +96,7 @@ public class SegmentedProgressBar extends View {
         int botY = getHeight();
 
         for (int i = 0; i < lastCompletedSegment; i++) {
-            canvas.drawRect(leftX, topY, rightX, botY, fillRectanglePaint);
+            drawRoundedRect(canvas, leftX, topY, rightX, botY, fillRectanglePaint);
             leftX = leftX + segmentWidth;
             rightX = rightX + segmentWidth;
         }
@@ -108,7 +109,44 @@ public class SegmentedProgressBar extends View {
         int rightX = leftX + currentSegmentProgressInPx;
         int topY = 0;
         int botY = getHeight();
-        canvas.drawRect(leftX, topY, rightX, botY, fillRectanglePaint);
+        drawRoundedRect(canvas, leftX, topY, rightX, botY, fillRectanglePaint);
+    }
+
+    private void drawRoundedRect(Canvas canvas, float left, float top, float right, float bottom, Paint paint) {
+
+        Path path = new Path();
+        float rx = 6;
+        if (rx < 0) rx = 0;
+        float ry = 6;
+        if (ry < 0) ry = 0;
+        float width = right - left;
+        float height = bottom - top;
+        if (rx > width / 2) rx = width / 2;
+        if (ry > height / 2) ry = height / 2;
+        float widthMinusCorners = (width - (2 * rx));
+        float heightMinusCorners = (height - (2 * ry));
+
+        path.moveTo(right, top + ry);
+        path.rQuadTo(0, -ry, -rx, -ry);//top-right corner
+        path.rLineTo(-widthMinusCorners, 0);
+        path.rQuadTo(-rx, 0, -rx, ry); //top-left corner
+        path.rLineTo(0, heightMinusCorners);
+
+        if (true) {
+            path.rLineTo(0, ry);
+            path.rLineTo(width, 0);
+            path.rLineTo(0, -ry);
+        } else {
+            path.rQuadTo(0, ry, rx, ry);//bottom-left corner
+            path.rLineTo(widthMinusCorners, 0);
+            path.rQuadTo(rx, 0, rx, -ry); //bottom-right corner
+        }
+
+        path.rLineTo(0, -heightMinusCorners);
+
+        path.close();//Given close, last lineto can be removed.
+
+        canvas.drawPath(path, paint);
     }
 
     private Paint buildFillRectanglePaint() {
@@ -120,9 +158,8 @@ public class SegmentedProgressBar extends View {
 
     private Paint buildContainerRectanglePaint() {
         Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(10);
-        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.GREEN);
+        paint.setStyle(Paint.Style.FILL);
         return paint;
     }
 }

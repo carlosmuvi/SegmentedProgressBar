c48788a4450ea74e275e1213b1d9c285a5f9bf47
Vinu <vinu@tricta.com>
2021-10-28 13:48:59 +0530

    custom color for individual segments with set progress

--- library/src/main/java/com/carlosmuvi/segmentedprogressbar/PropertiesModel.kt
+++ library/src/main/java/com/carlosmuvi/segmentedprogressbar/PropertiesModel.kt
@@ -3,17 +3,29 @@ package com.carlosmuvi.segmentedprogressbar
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
+import android.util.Log
+import java.math.BigDecimal
 
-class PropertiesModel(context: Context, attrs: AttributeSet?) {
+class PropertiesModel(context: Context, attrs: AttributeSet?)
+{
 
     var segmentCount: Int = DEFAULT_SEGMENT_COUNT
     var containerColor: Int = Color.LTGRAY
     var fillColor: Int = Color.BLUE
     var segmentGapWidth: Int = context.dp(DEFAULT_SEGMENT_GAP_DP)
     var cornerRadius: Int = context.dp(DEFAULT_CORNER_RADIUS_DP)
+    var progressValuePerContainer: Int = 50;
+    var progressFilledContainers: BigDecimal = BigDecimal("0.0")
+    var progressValue: Float = 0f
 
-    init {
-        if (attrs != null) {
+    init
+    {
+
+
+        if (attrs != null)
+        {
+
+
             val styledAttrs =
                     context.theme.obtainStyledAttributes(attrs, R.styleable.SegmentedProgressBar, 0, 0)
             segmentCount =
@@ -26,10 +38,31 @@ class PropertiesModel(context: Context, attrs: AttributeSet?) {
                     styledAttrs.getDimensionPixelSize(R.styleable.SegmentedProgressBar_gap_size, segmentGapWidth)
             cornerRadius =
                     styledAttrs.getDimensionPixelSize(R.styleable.SegmentedProgressBar_corner_radius, cornerRadius)
+            val totalProgress: Int = styledAttrs.getInt(R.styleable.SegmentedProgressBar_total_progress, 100)
+
+            Log.d("hefere", "progressValue : " + progressValue)
+            Log.d("hefere", "segmentCount : " + segmentCount)
+
+            progressValue = styledAttrs.getFloat(R.styleable.SegmentedProgressBar_progress, 0f)
+
+            calculateContainerValue(totalProgress)
+
+
         }
+
     }
 
-    companion object {
+    fun calculateContainerValue(totalProgress: Int = 100)
+    {
+
+        progressValuePerContainer = totalProgress / segmentCount;
+
+        if (progressValue != 0f)
+            progressFilledContainers = BigDecimal((progressValue / progressValuePerContainer).toString())
+    }
+
+    companion object
+    {
         const val DEFAULT_SEGMENT_COUNT = 5
         const val DEFAULT_CORNER_RADIUS_DP = 6
         const val DEFAULT_SEGMENT_GAP_DP = 1
